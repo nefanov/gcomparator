f7302d9d53f879c6d95c35a2f40697f0e862b0df
nefanov <nefanov90@gmail.com>
2020-11-05 02:05:50 +0300

    Initial commit: localized dist hash for sid

--- WL.py
+++ WL.py
@@ -2,46 +2,31 @@ import networkx as nx
 import hashing_algo as hash
 
 G1 = nx.Graph()
-
-
-G1.add_nodes_from([1,2])
-
+G1.add_nodes_from([1,2,3,4])
+G1.add_edge(1,3)
 G1.add_edge(1,2)
-G1.nodes[1]["label"]={'pid': 1,
-                    'sid': 1,
-                                            'ppid': 0,
-                                            }
-
-G1.nodes[2]["label"]={'pid': 2,
-                    'sid': 1,
-                                            'ppid': 1,
-                                            }
+G1.add_edge(2,4)
+G1.nodes[1]["label"]={'sid_dist': 0}
+G1.nodes[2]["label"]={'sid_dist': 1}
+G1.nodes[3]["label"]={'sid_dist': 1}
+G1.nodes[4]["label"]={'sid_dist': 2}
 
 G2 = nx.Graph()
-G2.add_nodes_from([1,2])
 
+G2.add_nodes_from([1,2,3,4])
+G2.add_edge(1,3)
 G2.add_edge(1,2)
-G2.nodes[1]["label"]={'pid': 1,
-                    'sid': 1,'ppid': 0,
-                                            }
+G2.add_edge(3,4)
+G2.nodes[1]["label"]={'sid_dist': 0}
+G2.nodes[2]["label"]={'sid_dist': 1}
+G2.nodes[3]["label"]={'sid_dist': 1}
+G2.nodes[4]["label"]={'sid_dist': 2}
 
-G2.nodes[2]["label"]={'pid': 2,
-                    'sid': 1, 'ppid': 1,}
 
-
-G3 = nx.Graph()
-G3.add_nodes_from([1,2])
-
-G3.add_edge(1,2)
-G3.nodes[1]["label"]={'pid': 1,
-                    'sid': 1,'ppid': 0,
-                                            }
-
-G3.nodes[2]["label"]={'pid': 20,
-                    'sid': 1, 'ppid': 1,}
+d1 = hash.weisfeiler_lehman_graph_hash(G1,node_attr="label")
+d2 = hash.weisfeiler_lehman_graph_hash(G2,node_attr="label")
+print("C =", abs(int(d1,16)-int(d2,16)))
 
 
 
-print(hash.weisfeiler_lehman_graph_hash(G1,node_attr="label"))
-print(hash.weisfeiler_lehman_graph_hash(G2,node_attr="label"))
-print(hash.weisfeiler_lehman_graph_hash(G3,node_attr="label"))
+
---
+++ test.py
@@ -0,0 +1,8 @@
+def func(i=1):
+    if i>1:
+        print(i)
+    else:
+        print(i*i)
+
+        for j in range(100):
+            print(j)
